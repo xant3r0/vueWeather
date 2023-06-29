368b808773e0459dd51f303df122059f363b06bf
xant3r0 <scandiba13@gmail.com>
2023-06-29 14:08:41 +0300

    Currently shows at moment situation

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,24 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+        <Main></Main>
 </template>
 
+<script>
+import Main from './components/Main.vue';
+export default {
+    components: {
+        Main
+    }
+}
+</script>
+
 <style>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+    display: flex;
+    justify-content: center;
 }
 </style>
---
+++ src/components/Main.vue
@@ -0,0 +1,166 @@
+<template>
+    <div id="main">
+        <div id="norm" v-if="clouds < 30 && currentTemp > 18 && currentState !== 'Mist'">
+            <button @click="getData" id="Search" for="City">
+                <img v-bind:src="require('../assets/Search.png')" alt="">
+            </button> 
+            <div id="data">
+                <input id="City" type="text" v-model="currentInputCity" :placeholder="placeholder"> 
+                <h1>{{ currentTemp }} °</h1>
+                <p id="State">{{ currentState }}</p>
+            </div>
+        </div>
+        <div id="frig" v-else-if="currentState === Mist">
+            <button @click="getData" id="Search" for="City">
+                <img v-bind:src="require('../assets/Search.png')" alt="">
+            </button> 
+            <div id="data">
+                <input id="City" type="text" v-model="currentInputCity" :placeholder="placeholder"> 
+                <h1>{{ currentTemp }} °</h1>
+                <p id="State">{{ currentState }}</p>
+            </div>
+        </div>
+        <div id="zero" v-else-if="currentTemp === 0">
+            <button @click="getData" id="Search" for="City">
+                <img v-bind:src="require('../assets/Search.png')" alt="">
+            </button> 
+            <div id="data">
+                <input id="City" type="text" v-model="currentInputCity" :placeholder="placeholder"> 
+                <h1>{{ currentTemp }} °</h1>
+                <p id="State">{{ currentState }}</p>
+            </div>
+        </div>
+        <div id="subzero" v-else-if="currentTemp < 0">
+            <button @click="getData" id="Search" for="City">
+                <img v-bind:src="require('../assets/Search.png')" alt="">
+            </button> 
+            <div id="data">
+                <input id="City" type="text" v-model="currentInputCity" :placeholder="placeholder"> 
+                <h1>{{ currentTemp }} °</h1>
+                <p id="State">{{ currentState }}</p>
+            </div>
+        </div>
+        <div id="frig" v-else="">
+            <button @click="getData" id="Search" for="City">
+                <img v-bind:src="require('../assets/Search.png')" alt="">
+            </button> 
+            <div id="data">
+                <input id="City" type="text" v-model="currentInputCity" :placeholder="placeholder"> 
+                <h1>{{ currentTemp }} °</h1>
+                <p id="State">{{ currentState }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+    data() {
+        return {
+            currentTemp:0,
+            currentState:'',
+            currentCity:'',
+            currentInputCity:'',
+            placeholder:'Enter a city',
+            clouds:0
+        }
+    },
+    methods: {
+        async getData() {
+            try {
+                const test = await axios.get('http://api.weatherapi.com/v1/forecast.json?key=386ff44423524ec89a2114131232806&q=' + this.currentInputCity + '&days=1&aqi=no&alerts=no');
+                this.currentTemp = test.data.current.feelslike_c;
+                this.currentState = test.data.current.condition.text;
+                this.currentCity = test.data.location.name;
+                console.log(test);
+            } catch(e) {
+            };
+        }
+    }
+}
+</script>
+
+<style>
+    * {
+        font-family: 'Ubuntu', sans-serif;
+    }
+    #norm {
+        background: rgb(2,0,36);
+        background: linear-gradient(45deg, rgba(2,0,36,1) 0%, rgba(241,67,97,1) 0%, rgba(238,190,85,1) 100%);
+        height:50vh;
+        width:100vw;
+        display:flex;
+        flex-direction: row-reverse;
+        justify-content: center;
+    }
+    h1 {
+        margin-top:10vh;
+        font-weight: 500;
+        font-size:72pt;
+        color:#FFFFFF;
+    }
+    #State {
+        margin-top:10vh;
+        font-size: 23pt;
+        color:#FFFFFF;
+    }
+    #City {
+        font-size: 23pt;
+        margin-top: 5vh;
+        border:none;
+        background:transparent;
+        text-align:center;
+        color:#FFFFFF;
+    }
+    #City:focus-visible {
+        border:none;
+        background:transparent;
+        outline: none;
+        text-align:center;
+        color:#FFFFFF;
+    }
+    #City::placeholder {
+        color:#FFFFFF;
+    }
+    #Search {
+        border:none;
+        background: transparent;
+        height: 5vh;
+        width:5vh;
+        margin-top: 5vh;
+        padding:0;
+    }
+    #data {
+        display:flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    #frig {
+        background: rgb(2,0,36);
+        background: linear-gradient(167deg, rgba(2,0,36,1) 0%, rgba(67,132,241,1) 0%, rgba(14,85,110,1) 100%);
+        height:50vh;
+        width:100vw;
+        display:flex;
+        flex-direction: row-reverse;
+        justify-content: center;
+    }
+    #zero {
+        background: rgb(2,0,36);
+        background: linear-gradient(324deg, rgba(2,0,36,1) 0%, rgba(172,185,208,1) 0%, rgba(178,194,199,1) 100%);
+        height:50vh;
+        width:100vw;
+        display:flex;
+        flex-direction: row-reverse;
+        justify-content: center;
+    }
+    #subzero {
+        background: rgb(2,0,36);
+        background: linear-gradient(324deg, rgba(2,0,36,1) 0%, rgba(172,185,208,1) 0%, rgba(178,194,199,1) 100%);
+        height:50vh;
+        width:100vw;
+        display:flex;
+        flex-direction: row-reverse;
+        justify-content: center;
+    }
+</style>
